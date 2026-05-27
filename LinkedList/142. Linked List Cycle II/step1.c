7de7afefa01f059b14c542463590bc767ed88d15
Taka0x00 <sandabulk@gmail.com>
2026-05-27 21:20:41 +0900

    142

---
+++ LinkedList/142. Linked List Cycle II/step1.c
@@ -0,0 +1,24 @@
+struct ListNode* detectCycle(struct ListNode* head)
+{
+    struct ListNode* currentPoint = head;
+    struct ListNode* visited[10000] = {0};
+    int visitedCounter=0;
+
+
+    while(currentPoint != NULL)
+    {
+        for(int i=0;i<visitedCounter;i++)
+        {
+            if(visited[visitedCounter] == currentPoint)
+            {
+                return currentPoint;
+            }
+        }
+
+        visited[visitedCounter] = currentPoint;
+        visitedCounter++;
+        currentPoint = currentPoint->next;
+    }
+
+    return NULL;
+}
