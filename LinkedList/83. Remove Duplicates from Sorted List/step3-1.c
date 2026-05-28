d3714cb3e27e76bffeac4fc4eb3b6e06c96e5356
Taka0x00 <sandabulk@gmail.com>
2026-05-28 15:10:45 +0900

    step3-1

---
+++ LinkedList/83. Remove Duplicates from Sorted List/step3-1.c
@@ -0,0 +1,28 @@
+struct ListNode* deleteDuplicates(struct ListNode* head)
+{
+    struct ListNode* currentPoint = head;
+
+    if(currentPoint == NULL)
+    {
+        return NULL;
+    }
+
+    if(currentPoint->next == NULL)
+    {
+        return head;
+    }
+
+    while(currentPoint->next != NULL)
+    {
+        if(currentPoint->next->val == currentPoint->val)
+        {
+            currentPoint->next = currentPoint->next->next;
+        }
+        else
+        {
+            currentPoint = currentPoint->next;
+        }
+    }
+
+    return head;
+}
